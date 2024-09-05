46099ce9f1ced858c7ec38fc7de2849afb1f9253
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-05 11:08:23 -0500

    added hunger

--- emmy.rb
+++ emmy.rb
@@ -1,11 +1,20 @@
 class Emmy
 
-    attr_accessor :is_hungry
+    attr_accessor :is_hungry, fed
 
     attr_reader :attitude
 
     def initialize(attitude, is_hungry)
         @attitude = attitude
         is_hungry = true 
+        @fed = fed
+    end
+
+    def hungry?
+        if is_hungry
+            true
+        else @fed
+            false
+        end
     end
 end
