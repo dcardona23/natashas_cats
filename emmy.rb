827a074c06bc1bfc487089640754ca58ff03b9ae
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-05 09:58:54 -0600

    initial commit

---
+++ emmy.rb
@@ -0,0 +1,11 @@
+class Emmy
+
+    attr_accessor :is_hungry
+
+    attr_reader :attitude
+
+    def initialize(attitude, is_hungry)
+        @attitude = attitude
+        is_hungry = true 
+    end
+end
